5c9034bf1f693312304e8ade2f7adc063495a2ea
Sammy <sammylam_1@hotmail.com>
2023-01-27 18:04:44 -0500

    firebase for history

--- src/component/History.js
+++ src/component/History.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useUserAuth } from '../context/UserAuthContext';
 
 export default function History() {
+	const { user } = useUserAuth();
+	const getFirebaseData = () => {
+		const db = getDatabase(firebaseConfig);
+		const dbRef = ref(db, `user/${user.uid}/`);
+		onValue(dbRef, (snapshot) => {
+			const data = snapshot.val();
+			// console.log('snapshot:', data);
+		});
+	};
 	return <div></div>;
 }
